cbe4050d09a5a8992864cec3005d40f583986782
SunderApps <sam@sunderapps.com>
2020-04-15 10:36:49 -0400

    Added Days 2&3

--- src/app/workouts/workouts.component.ts
+++ src/app/workouts/workouts.component.ts
@@ -151,9 +151,9 @@ export class WorkoutsComponent implements OnInit {
         },
         {
           name: "PM",
+          description: "Static stretching",
           reps: [
             {
-              name: "Static stretching",
               exercises: [
                 {
                   amount: "1 minute",
@@ -172,7 +172,274 @@ export class WorkoutsComponent implements OnInit {
           ]
         }
       ]
-    }
+    },
+    {
+      theme: "CONTAIN",
+      description: "Pull strength, Isometrics, Core, & Conditioning",
+      sections: [
+        {
+          name: "Warm up",
+          reps: [
+            {
+              exercises: [
+                {
+                  name: "Yoga Flow"
+                },
+                {
+                  amount: "10",
+                  name: "PVC pipe or band shoulder pass throughs"
+                },
+                {
+                  amount: "10 per direction, per side",
+                  name: "PVC pipe circles"
+                },
+                {
+                  amount: "5 per side",
+                  name: "Fire hydrants"
+                },
+                {
+                  amount: "5 per direction, per side",
+                  name: "Kneeling hip circles"
+                },
+                {
+                  amount: "5 per side",
+                  name: "Birddogs w/ 2 second pause"
+                },
+                {
+                  amount: "10",
+                  name: "Lying cross body kicks"
+                }
+              ]
+            }
+          ]
+        },
+        {
+          name: "Flesh eating muscle mayhem",
+          reps: [
+            {
+              amount: "x5",
+              exercises: [
+                {
+                  amount: "Max",
+                  name: "(Weighted) pull ups"
+                },
+                {
+                  name: "Shoulder press"
+                }
+              ]
+            },
+            {
+              amount: "x4",
+              exercises: [
+                {
+                  amount: "6 per side",
+                  name: "Single Leg Cross - Body KB Deadlift"
+                },
+                {
+                  amount: "6 per side",
+                  name: "Single Arm KB Snatches"
+                }
+              ]
+            },
+            {
+              amount: "x3",
+              exercises: [
+                {
+                  amount: "10",
+                  name: "Skull crushers w/ dragon flag"
+                },
+                {
+                  name: "AMAP L-Sit Chin Up"
+                }
+              ]
+            },
+            {
+              amount: "x2",
+              exercises: [
+                {
+                  amount: "8",
+                  name: "Reverse grip bench press or push ups"
+                },
+                {
+                  amount: "8",
+                  name: "Bent over KB rows"
+                }
+              ]
+            },
+            {
+              exercises: [
+                {
+                  amount: "Max",
+                  name: "Handstand hold"
+                },
+                {
+                  amount: "50",
+                  name: "Goblin squats"
+                }
+              ]
+            }
+          ]
+        },
+        {
+          name: "Rest 3-5 minutes",
+          reps: [
+            {
+              exercises: [
+                {
+                  amount: "3 minutes Max",
+                  name: "Burpees"
+                }
+              ]
+            }
+          ]
+        },
+        {
+          name: "PM",
+          reps: [
+            {
+              exercises: [
+                {
+                  amount: "5 minutes",
+                  name: "Grip strength trainer"
+                },
+                {
+                  amount: "10 minutes",
+                  name: "Static stretches"
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    },
+    {
+      theme: "EXTERMINATE",
+      description: "Run for your life",
+      sections: [
+        {
+          name: "Warm up",
+          reps: [
+            {
+              exercises: [
+                {
+                  name: "Alternating side lunges"
+                },
+                {
+                  name: "Walking lunge w/ hip circles"
+                },
+                {
+                  amount: "10 min",
+                  name: "Light job"
+                }
+              ]
+            }
+          ]
+        },
+        {
+          name: "Zombie sprints (outdoors or on treadmill)",
+          reps: [
+            {
+              amount: "x6",
+              exercises: [
+                {
+                  amount: "30 sec",
+                  name: "Max effort"
+                },
+                {
+                  amount: "2 min",
+                  name: "Walk"
+                }
+              ]
+            },
+            {
+              exercises: [
+                {
+                  amount: "5 min",
+                  name: "Double under / jump rope skill practice"
+                },
+                {
+                  amount: "10 min",
+                  name: "Shadowboxing technique"
+                }
+              ]
+            }
+          ]
+        },
+        {
+          name: "PM",
+          description: "Bruce Lee Ab Circuit",
+          reps: [
+            {
+              amount: "x2",
+              exercises: [
+                {
+                  amount: "20",
+                  name: "V-ups"
+                },
+                {
+                  amount: "30 sec",
+                  name: "Hollow hold"
+                },
+                {
+                  amount: "20",
+                  name: "Scissor kicks"
+                },
+                {
+                  amount: "30",
+                  name: "Bicycle kicks"
+                },
+                {
+                  amount: "20",
+                  name: "Leg lifts"
+                },
+                {
+                  amount: "30",
+                  name: "Russian twists"
+                }
+              ]
+            },
+            {
+              amount: "x5",
+              exercises: [
+                {
+                  amount: "5",
+                  name: "Dragon flag"
+                }
+              ]
+            },
+            {
+              exercises: [
+                {
+                  name: "Static stretching"
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    }/*,
+    {
+      theme: "SURVIVE",
+      description: "Yoga & Breathing",
+      sections: [
+        {
+          name: "Yoga",
+          reps: [
+            {
+              amount: "1 hour",
+              exercises: [
+                {
+                  name: "Start sitting cross legged"
+                },
+                {
+                  name: ""
+                },
+              ]
+            }
+          ]
+        }
+      ]
+    }*/
   ]
   constructor() { }
 
