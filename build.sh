18d05efac858ffe907aa20930ac9ba4f1a909a57
SunderApps <sam@sunderapps.com>
2020-04-28 21:06:42 -0400

    Fixed Base Href

--- build.sh
+++ build.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 ng build --prod --base-href=./
 cp ./docs/index.html ./docs/404.html
-echo 28repslater.deathcomeslifting.com > ./docs/CNAME
 rm -r ../dcl-reps-cv/www
 mkdir ../dcl-reps-cv/www
 cp ./docs/* ../dcl-reps-cv/www
 cd ../dcl-reps-cv
 cordova build --release ios
 cordova build --release android
+cd ../dcl-reps-ng
+ng build --prod --base-href=/
+cp ./docs/index.html ./docs/404.html
+echo 28repslater.deathcomeslifting.com > ./docs/CNAME
